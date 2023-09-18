72f26bdd9b5188003c325efe9e39e2c437aadc8e
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-09-18 23:31:19 +0530

    Stack setup inital commit

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button"
+import { getUserAuth } from "@/lib/auth/utils";
+import { UserButton } from "@clerk/nextjs";
+
+export default async function Home() {
+  const userAuth = await getUserAuth();
+  return (
+    <main className="">
+      <UserButton afterSignOutUrl="/" />
+      <pre>{JSON.stringify(userAuth, null, 2)}</pre>
+      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-bold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+          Beautifully designed components <br className="hidden sm:inline" />
+          built with Radix UI and Tailwind CSS.
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
+          Accessible and customizable components that you can copy and paste
+          into your apps. Free. Open Source. And Next.js 13 Ready.
+        </p>
+      </div>
+      <form>
+        <Button>Submit</Button>
+      </form>
+    </section>
+    </main>
+  );
+}
