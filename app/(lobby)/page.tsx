22e477135d37b4293bb89779ee0911c2f2c233b9
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-09-27 23:28:31 +0530

    Hero section created

--- app/(lobby)/page.tsx
+++ app/(lobby)/page.tsx
@@ -1,12 +1,77 @@
-
+import { Icons } from "@/components/icons";
+import { Shell } from "@/components/shells/shell";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { toast } from "sonner";
 
 export default function IndexPage() {
-
   return (
     <div>
-      
+      <section className="w-full py-12 md:py-24 lg:py-32 xl:py-24 bg-background ">
+        <div className="container px-4 md:px-6">
+          <div className="grid gap-6 items-center">
+            <div className="flex flex-col justify-center space-y-8 text-center">
+              <div className="space-y-2">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
+                  Create Products. Engage Fans. Make Money.
+                </h1>
+                <p className="max-w-[600px] text-foreground mx-auto ">
+                  Turn your ideas into physical and digital products that your
+                  fans will love.
+                </p>
+              </div>
+              <div className="w-full max-w-full space-y-4 mx-auto">
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-8">
+                  <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg">
+                    <div className="p-2 bg-background  bg-opacity-50 rounded-full">
+                      <Icons.edit />
+                    </div>
+                    <h2 className="text-xl font-bold text-white">
+                      Create physical & digital products
+                    </h2>
+                    <p className="text-zinc-200 dark:text-zinc-100">
+                      Customize 180+ products, sell digital items, or create
+                      custom goods. We manage fulfillment and the entire fan
+                      experience.
+                    </p>
+                  </div>
+                  <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg">
+                    <div className="p-2 bg-background  bg-opacity-50 rounded-full">
+                      <Icons.send />
+                    </div>
+                    <h2 className="text-xl font-bold text-white">
+                      Launch a Free Store
+                    </h2>
+                    <p className="text-zinc-200 dark:text-zinc-100">
+                      Sell products in your personalized, conversion-optimized
+                      store. Connect a custom domain and be ready to promote in
+                      minutes.
+                    </p>
+                  </div>
+                  <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg">
+                    <div className="p-2 bg-background  bg-opacity-50 rounded-full">
+                      <Icons.wallet />
+                    </div>
+                    <h2 className="text-xl font-bold text-white">
+                      Sell More Merch
+                    </h2>
+                    <p className="text-zinc-200 dark:text-zinc-100">
+                      Create on Harbor X, connect your social channels and sell
+                      more merch by allowing your fans to shop exactly where
+                      they are.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <Button asChild className="text-xl" variant={"default"}>
+              <Link href={"/create"}>Start Creating, its free</Link>
+            </Button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
