564d9ceca6500778c12ba130c19f6a1fee8f442d
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-10-07 16:12:36 +0530

    some fix

--- packages/api/trpc/utils.ts
+++ packages/api/trpc/utils.ts
@@ -1,7 +1,7 @@
 import { env } from "@harborx/auth";
 export function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  if (env.server.NEXT_PUBLIC_APP_URL) return `https://${env.server.NEXT_PUBLIC_APP_URL}`;
+  if (env.NEXT_PUBLIC_APP_URL) return `https://${env.NEXT_PUBLIC_APP_URL}`;
   return "http://localhost:3000";
 }
 
