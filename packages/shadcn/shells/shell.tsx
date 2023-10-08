97044d67bd7efb0a87b19521545d2dc41293116b
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-10-08 22:28:20 +0530

    had to make this seperate pacakge for shadcn cuz for simplicity and fixing things

---
+++ packages/shadcn/shells/shell.tsx
@@ -0,0 +1,32 @@
+import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+import { cn } from "@harborx/types/utils"
+
+const shellVariants =  cva("grid items-center gap-8 pb-8 pt-6 md:py-8", {
+    variants: {
+        variant:{
+            default:"container",
+            sidebar:"",
+            centered:"container flex h-[100dvh] max-w-2xl flex-col justify-center",
+            markdown:"container max-w-3xl gap-0 py-8 md:py-10 lg:py-10"
+        }
+    },
+    defaultVariants:{
+        variant:"default"
+    }
+})
+
+interface ShellProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof shellVariants>{
+    as?: React.ElementType
+}
+
+function Shell({
+    className,
+    as: Component = "section",
+    variant,
+    ...props
+}: ShellProps){
+    return <Component className= {cn(shellVariants({variant}), className)} {...props}></Component>
+}
+
+export {Shell, shellVariants}
