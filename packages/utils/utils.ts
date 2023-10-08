e785bd2906a0aa50747edee5a4420a91d5d6d7b8
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-10-09 03:54:03 +0530

    exporting utils which were to be used in app

--- packages/utils/utils.ts
+++ packages/utils/utils.ts
@@ -1,6 +1,9 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
+export { cva, type VariantProps } from "class-variance-authority";
+export {Balancer} from "react-wrap-balancer";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
