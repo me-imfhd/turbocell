3b4b4fb7b247219f72e1e746812577822190410a
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-10-15 13:48:42 +0530

    added zod schema for login and signup inputs

--- packages/db/schema/auth.ts
+++ packages/db/schema/auth.ts
@@ -1,6 +1,6 @@
 // we do not need this if we are using next-auth or other auth-service providers
 
-// since our 
+// since our
 import { z } from "zod";
 import { userSchema } from "../prisma/zod";
 
@@ -8,3 +8,23 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8).max(16),
 });
+
+export const signupSchema = loginSchema.extend({
+  name: z.string().min(3).max(24),
+});
+
+export const updateUserSchema = z.object({
+  email: z.string().email(),
+  name: z.string().min(3).max(24),
+  image: z.string().optional(),
+});
+
+export const updatePasswordSchema = z
+  .object({
+    password: z.string().min(8).max(16),
+    confirmPassword: z.string().min(8).max(16),
+  })
+  .refine((schema) => schema.password === schema.confirmPassword, {
+    message: "Password does not match",
+    path: ["confirmPassword"],
+  });
