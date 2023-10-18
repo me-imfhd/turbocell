1cdda453f8edf71227fc99e058471764192864b3
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-10-18 23:42:38 +0530

    some fix

--- apps/express-server/src/route/auth/updateUser.ts
+++ apps/express-server/src/route/auth/updateUser.ts
@@ -28,12 +28,14 @@ router.post("/updateImage", (req, res) => {
       const finalFile: formidable.File = file[0];
       const base64Image = encodeImageToBase64(finalFile.filepath);
 
-      validateImage(req, res, finalFile);
-
+      const result = validateImage(req, res, finalFile);
+      fs.unlinkSync(finalFile.filepath);
+      if(!result){
+        return;
+      }
       res.send(`
         <div style="margin-bottom: 10px;"><img src=${base64Image} style="width: 400px; height:400px; padding: 10px; border: 1px solid lightgray; border-radius: 5px;"></div>
         `);
-      fs.unlinkSync(finalFile.filepath);
     });
   } catch (err) {
     res.json({
@@ -48,43 +50,47 @@ router.get("/updateUser", (req: Request, res: Response) => {
   res.send(`
   <h2 style="text-align: center; color: slategray; padding: 20px; margin-bottom: 30px;">Update Profile</h2>
   <form action="${updateImageEndpoint}" enctype="multipart/form-data" method="post" style="width: 400px; margin: 0 auto; background-color: white; padding: 20px; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
-    <div style="margin-bottom: 10px;">File: <input type="file" name="someExpressFiles" multiple="multiple" style="width: 100%; padding: 10px; border: 1px solid lightgray; border-radius: 5px;"></div>
-    <input type="submit" value="Upload" style="background-color: slategray; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">
+  <div style="margin-bottom: 10px;">File: <input type="file" name="someExpressFiles" multiple="multiple" style="width: 100%; padding: 10px; border: 1px solid lightgray; border-radius: 5px;"></div>
+  <input type="submit" value="Upload" style="background-color: slategray; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">
   </form>
   
-    `);
+  `);
 });
 
 function validateImage(req: Request, res: Response, file: formidable.File) {
-  const allowedMimeTypes = ["image/jpeg", "image/png", "image/webp"];
+  const allowedMimeTypes = ["image/jpeg", "image/png", "image/webp","image/jpg"];
   const finalFileMime = file.mimetype as string;
   const allowedExtensions = [".jpg", ".jpeg", ".png", ".webp"];
   const finalFileExt = ("." +
-    file.originalFilename?.split(".").at(-1)?.toLowerCase()) as string;
+  file.originalFilename?.split(".").at(-1)?.toLowerCase()) as string;
   const maxFileSize = 1 * 1024 * 1024; // 1 MB
   const finalFileSize = file.size;
-
+  
   if (!allowedMimeTypes.includes(finalFileMime)) {
-    return res.status(400).json({
+    res.status(400).json({
       status: "Fail",
       message:
-        "Invalid file type. Only JPEG, PNG, and WebP images are allowed.",
+      "Invalid file type. Only JPG, JPEG, PNG, and WebP images are allowed.",
     });
+    return false;
   }
   if (!allowedExtensions.includes(finalFileExt)) {
-    return res.status(400).json({
+    res.status(400).json({
       status: "Fail",
       message:
-        "Invalid file extension. Only JPEG, PNG, and WebP images are allowed.",
+      "Invalid file extension. Only JPG, JPEG, PNG, and WebP images are allowed.",
     });
+    return false;
   }
-
+  
   if (finalFileSize > maxFileSize) {
-    return res.status(400).json({
+    res.status(400).json({
       status: "Fail",
       message:
-        "File size exceeds the limit. Maximum file size allowed is 1 MB.",
+      "File size exceeds the limit. Maximum file size allowed is 1 MB.",
     });
+    return false;
   }
+  return true;
 }
 export { router as updateUserRoute };
