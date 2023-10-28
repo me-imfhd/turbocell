618706136310cb579482675aee4711b920eca431
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-10-28 23:09:53 +0530

    dockerfile for express app and modification in scripts...

--- apps/express-server/src/index.ts
+++ apps/express-server/src/index.ts
@@ -8,6 +8,6 @@ setupRoutes(server);
 
 server.listen(port, () => {
   console.log(
-    `api running on ${port}, http://localhost:${port}/loginViaBrowser , http://localhost:${port}/signupViaBrowser`
+    `Api running on ${port}, http://localhost:${port}/loginViaBrowser, http://localhost:${port}/signupViaBrowser`
   );
 });
