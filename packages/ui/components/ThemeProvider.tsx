b8801cf2bf4ba1506b9cd0e9f66d922502b8de65
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-04 14:10:55 +0530

    migrating from mysql to postgres db and some db:push fixes and some other fixes

--- packages/ui/components/ThemeProvider.tsx
+++ packages/ui/components/ThemeProvider.tsx
@@ -2,10 +2,17 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import {Provider} from "@radix-ui/react-tooltip";
+import { Provider } from "@radix-ui/react-tooltip";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+interface ThemeProviderWithChildrenProps extends ThemeProviderProps {
+  children: React.ReactNode;
+}
+
+export function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderWithChildrenProps) {
   return (
     <NextThemesProvider {...props}>
       <Provider>{children}</Provider>
