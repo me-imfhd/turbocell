ca0d35160db95f3f07125dac042037824a931012
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-14 05:25:29 +0530

    updated api with new modifications2

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -74,3 +74,14 @@ export const {
     }),
   ],
 });
+
+export async function getUser() {
+  try {
+    const session = await auth();
+    const user = session?.user;
+    return user;
+  } catch (err) {
+    console.error(err);
+    throw new Error((err as Error).message ?? "Error, please try again");
+  }
+}
