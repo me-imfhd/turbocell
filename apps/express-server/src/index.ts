f6c0d3d7c6b7716db36dd3f1dc44abb52d364fdb
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-17 17:46:00 +0530

    vanilla express app and one trpc-openapi-express app

--- apps/express-server/src/index.ts
+++ apps/express-server/src/index.ts
@@ -1,3 +1,4 @@
+import { createTRPCServer } from "openapi-trpc.server";
 import { setupRoutes } from "routes";
 import { createServer } from "server";
 
@@ -10,4 +11,10 @@ server.listen(port, () => {
   console.log(
     `Api running on ${port}, http://localhost:${port}/loginViaBrowser, http://localhost:${port}/signupViaBrowser`
   );
+});
+
+// express server whose routes are written in trpc in the api package inside /api/express-server/routes/_app.ts
+const trpcServer = createTRPCServer();
+trpcServer.listen(2024, () => {
+  console.log(`Api running on port 2024 `);
 });
