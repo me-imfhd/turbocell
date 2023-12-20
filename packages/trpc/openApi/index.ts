7b36aa55d58b0a781e09d1ce4a0f49e4747d0bef
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-21 00:13:34 +0530

    baseUrl fix

--- packages/trpc/openApi/index.ts
+++ packages/trpc/openApi/index.ts
@@ -5,7 +5,10 @@ export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: "Turbo Cell Open Api Swagger",
   description: "OpenAPI compliant REST API built using tRPC with Next.js",
   version: "1.0.0",
-  baseUrl: "http://localhost:3000",
+  baseUrl:
+    process.env.VERCEL_ENV === "production"
+      ? "https://turbocell-web.vercel.app"
+      : "http://localhost:3000",
   tags: ["auth", "computers"],
 });
 
