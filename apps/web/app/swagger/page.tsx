865e3e2bb0a575b6adcf9b4817f373d139d07dd1
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-21 13:38:49 +0530

    swaggerURI fix

--- apps/web/app/swagger/page.tsx
+++ apps/web/app/swagger/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 import React from "react";
+import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
-// import SwaggerUI from "swagger-ui-react";
+
+const swaggerURI =
+  process.env.VERCEL_ENV === "production"
+    ? "https://turbocell-web.vercel.app/api/openapi"
+    : "http://localhost:3000/api/openapi";
 
 export default function Page() {
   return (
     <div className="bg-white text-black">
       This will be the swagger page
-      {/* <SwaggerUI url="http://localhost:3000/api/openapi"></SwaggerUI> */}
+      <SwaggerUI url={swaggerURI}></SwaggerUI>
     </div>
   );
 }
