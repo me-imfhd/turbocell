056109ad4c4b86d3d568e4236a604ebf034bf714
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-21 13:30:29 +0530

    removing swagger page

--- apps/web/app/swagger/page.tsx
+++ apps/web/app/swagger/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React from "react";
 import "swagger-ui-react/swagger-ui.css";
-import SwaggerUI from "swagger-ui-react";
+// import SwaggerUI from "swagger-ui-react";
 
 export default function Page() {
   return (
-    <div className="bg-white">
-      <SwaggerUI url="http://localhost:3000/api/openapi"></SwaggerUI>
+    <div className="bg-white text-black">
+      This will be the swagger page
+      {/* <SwaggerUI url="http://localhost:3000/api/openapi"></SwaggerUI> */}
     </div>
   );
 }
