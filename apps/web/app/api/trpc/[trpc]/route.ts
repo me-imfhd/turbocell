994d07da79cd3ff32744d9a527ff48115140a93a
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-23 15:28:40 +0530

    added context in trpc handler and fixed whatever was required

--- apps/web/app/api/trpc/[trpc]/route.ts
+++ apps/web/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,15 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@turbocell/trpc";
+import { createContext } from "@turbocell/trpc/server/context";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = (req: Request) =>
-  fetchRequestHandler({
+const handler = async (req: NextApiRequest & Request, res: NextApiResponse) => {
+  const context = await createContext({ req, res });
+  return fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: () => ({}),
+    createContext: () => context,
   });
-
+};
 export { handler as GET, handler as POST };
