f5559097ae2288163a7e4cf5e5cba147c7ba4bdc
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-23 16:03:36 +0530

    req, res object fix

--- apps/web/app/api/[...trpc]/route.ts
+++ apps/web/app/api/[...trpc]/route.ts
@@ -1,9 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 // import { createOpenApiNextHandler } from "@turbocell/trpc/openApi";
 // import { appRouter } from "@turbocell/trpc";
 // import { createContext } from "@turbocell/trpc/server/context";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: Request | NextRequest,
+  res: Response | NextResponse
+) {
   return Response.json({ msg: "This is under development" });
   // const context = await createContext({ req, res });
   // return createOpenApiNextHandler({
