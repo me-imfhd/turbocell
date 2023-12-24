842fe1db3b7d002eb1d3e6cf654d5bfc7ac7a0fa
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-24 20:54:56 +0530

    createContextApi function for trpc rest api endpoint creation

--- packages/trpc/server/context.ts
+++ packages/trpc/server/context.ts
@@ -17,11 +17,27 @@ export async function createContextInner(opts: CreateInnerContextOptions) {
   };
 }
 
+// for RSC(app router) we need to send only authOptions for getting server session
 export async function createContext({
   req,
   res,
 }: trpcNext.CreateNextContextOptions) {
   const session = await getServerSession(authOptions);
+  // const session = null;
+  const contextInner = await createContextInner({ session });
+  return {
+    ...contextInner,
+    req,
+    res,
+  };
+}
+
+// for api routes we need to send req and res objects to get session
+export async function createContextApi({
+  req,
+  res,
+}: trpcNext.CreateNextContextOptions) {
+  const session = await getServerSession(req, res, authOptions);
   // const session = null;
   const contextInner = await createContextInner({ session });
   return {
