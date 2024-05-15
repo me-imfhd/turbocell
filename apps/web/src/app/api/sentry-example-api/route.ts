2eb0409d461f45b93ce5854f4d5570d99c50bbb7
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-05-16 00:52:32 +0530

    setup sentry for error management

---
+++ apps/web/src/app/api/sentry-example-api/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+// A faulty API route to test Sentry's error monitoring
+export function GET() {
+  throw new Error("Sentry Example API Route Error");
+  // @ts-ignore
+  return NextResponse.json({ data: "Testing Sentry Error..." });
+}
