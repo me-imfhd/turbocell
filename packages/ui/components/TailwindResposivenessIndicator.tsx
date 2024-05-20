31b867de1b0b29cad21f1084b10bf41b95d7f6ee
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-05-21 05:00:54 +0530

    taiwind responsive indicator only developmental

--- packages/ui/components/TailwindResposivenessIndicator.tsx
+++ packages/ui/components/TailwindResposivenessIndicator.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const TailwindResposivenessIndicator = () => {
+  if (process.env.NODE_ENV === "production") {
+    return;
+  }
   return (
     <div className="fixed bottom-1 left-1 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
       <div className="block sm:hidden">xs</div>
@@ -12,7 +15,7 @@ const TailwindResposivenessIndicator = () => {
       <div className="hidden xl:block 2xl:hidden">xl</div>
       <div className="hidden 2xl:block">2xl</div>
     </div>
-  )
-}
+  );
+};
 
-export default TailwindResposivenessIndicator
+export default TailwindResposivenessIndicator;
