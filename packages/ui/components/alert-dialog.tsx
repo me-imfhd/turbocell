a8429615c50bf5d7d5736692d4988be551ab4707
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-05-20 22:24:13 +0530

    use sonner toasts, update button for loading

--- packages/ui/components/alert-dialog.tsx
+++ packages/ui/components/alert-dialog.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 import { cn } from "../cn"
-import { buttonVariants } from "./button"
+import { buttonVariants } from "./button-variants"
 
 
 const AlertDialog = AlertDialogPrimitive.Root
--- packages/ui/components/button.tsx
+++ packages/ui/components/button.tsx
@@ -1,37 +1,8 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
+import { type VariantProps } from "class-variance-authority";
 import { cn } from "../cn";
-
-const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-  {
-    variants: {
-      variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
-      },
-      size: {
-        default: "h-10 px-4 py-2",
-        xs: "h-6 px-2 rounded-full text-xs", // pill-shaped
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
-    },
-  }
-);
+import { buttonVariants } from "./button-variants";
+import { Loader2 } from "lucide-react";
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -43,22 +14,63 @@ export interface ButtonProps
     | "link"
     | "outline"
     | "secondary";
+  animationType?: "none" | "up" | "down" | "right" | "left";
+  disabled?: boolean;
+  tap?: "default" | "in" | "out";
   size?: "default" | "icon" | "lg" | "sm" | "xs";
   asChild?: boolean;
+  withArrow?: boolean;
+  buttonIcon?: React.ReactNode;
+  iconDirection?: "left" | "right";
+  isLoading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
+  (
+    {
+      children,
+      className,
+      tap,
+      variant,
+      size,
+      asChild = false,
+      buttonIcon = <></>,
+      iconDirection = "left",
+      withArrow = false,
+      disabled = false,
+      isLoading = false,
+      animationType,
+      ...props
+    },
+    ref
+  ) => {
+    const buttonClassNames = cn(
+      buttonVariants({ variant, size, className, animationType, tap }),
+      !disabled && "hover:brightness-110 active:brightness-90",
+      disabled && "cursor-not-allowed opacity-40",
+      isLoading && "cursor-default opacity-60 transition-all"
+    );
     return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+      <button
+        className={buttonClassNames}
         ref={ref}
+        disabled={disabled}
         {...props}
-      />
+      >
+        <span className="button-content-wrapper flex items-center gap-2">
+          {isLoading && (
+            <span key={"loader-wrapper"} className="hover:translate-x-3">
+              <Loader2 className="h-4 w-4 animate-spin" />
+            </span>
+          )}
+          {iconDirection === "left" && buttonIcon}
+          {children}
+          {iconDirection === "right" && buttonIcon}
+        </span>
+      </button>
     );
   }
 );
 Button.displayName = "Button";
 
-export { Button, buttonVariants };
+export { Button };
